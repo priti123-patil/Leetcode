882bacc19b01e6c769a031901aa5906967ec512f
priti123-patil <68500888+priti123-patil@users.noreply.github.com>
2022-10-14 22:35:12 +0530

    Time: 1 ms (94.10%), Space: 42.2 MB (43.84%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String sortSentence(String s) {
+        
+        String[] words = s.split(" ");
+        
+        String[] ans = new String[words.length];
+        
+        for (String word : words) 
+        {
+            int i = word.length() - 1;
+            
+            ans[word.charAt(i) - '1'] = word.substring(0, i);
+        }
+        
+        
+        return String.join(" ", ans);
+        
+    }
+}
