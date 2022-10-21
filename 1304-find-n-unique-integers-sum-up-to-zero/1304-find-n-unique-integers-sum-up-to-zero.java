897a7c696efad39c074e89849a3803a1a3af9043
priti123-patil <68500888+priti123-patil@users.noreply.github.com>
2022-10-21 20:06:46 +0530

    Time: 1 ms (42.14%), Space: 42.4 MB (31.46%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
@@ -3,20 +3,11 @@ class Solution {
     {
         int[] res=new int[n];
         
-        if(n==1)
-        {
-            res[0]=0;
-            return res;
-        }
-        
         for(int i=0 ;i < n-1 ;i+=2)
         {
             res[i]  =   i+1;
             res[i+1]= -(i+1);
         }
-        
-        if(n%2!=0)
-            res[n-1]=0;
         
         return res;
     }
