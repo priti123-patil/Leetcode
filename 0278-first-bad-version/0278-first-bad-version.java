50ff864069b5bbca56382f173790212397ed2bb6
priti123-patil <68500888+priti123-patil@users.noreply.github.com>
2022-10-25 09:50:32 +0530

    Time: 26 ms (45.27%), Space: 41.2 MB (16.98%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,19 +3,18 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        
+    
         int start=1;
         int end=n;
         int mid=0;
         while(start<=end)
         {
             mid=start+(end-start) /2;
+            
             if(isBadVersion(mid))
             {
-                if( !isBadVersion(mid-1))
-                {
-                     return mid;
-                }
+                if(!isBadVersion(mid-1))
+                    return mid;
                 end=mid-1;
             }
             else
@@ -23,6 +22,6 @@ public class Solution extends VersionControl {
                 start=mid+1;
             }
         }
-        return 0;
+        return mid;
     }
 }
