fac8ff8ec1751a5c9addc7286e47040d282e3397
priti123-patil <68500888+priti123-patil@users.noreply.github.com>
2022-11-11 19:42:28 +0530

    Time: 85 ms (23.57%), Space: 82 MB (5.05%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        Map<Integer, List<Integer>> map = new TreeMap<>();
+        for (int i = 0; i < arr.length; i++) 
+        {
+			map.computeIfAbsent(arr[i], k -> new ArrayList<>()).add(i);
+        }
+        int rank = 1;
+        
+        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) 
+        {
+            List<Integer> currentList = entry.getValue();
+            for(int i: currentList)  arr[i] = rank;
+            rank++;
+        }
+        return arr;
+    }
+}
