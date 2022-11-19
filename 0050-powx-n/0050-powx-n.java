10f246802f34faba7e53fea8b547567d34ff8789
priti123-patil <68500888+priti123-patil@users.noreply.github.com>
2022-11-19 21:42:55 +0530

    Time: 1 ms (87.63%), Space: 42.5 MB (67.87%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double ans = 1;
+        
+        long absn = Math.abs((long)n);
+        
+        while(absn > 0) 
+        {
+            if((absn&1)==1)
+            {
+                ans *= x;
+            } 
+            absn >>= 1;
+            x *= x;
+        }
+        return n < 0 ?  1/ans : ans;
+    }
+}
