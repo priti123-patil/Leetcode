cc2af4796d415f80d22dec0d6f64a3e744916dd6
priti123-patil <68500888+priti123-patil@users.noreply.github.com>
2023-01-25 23:47:34 +0530

    Time: 1 ms (99.73%), Space: 43.8 MB (46.32%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int[] kWeakestRows(int[][] mat, int k) {
+        int r = mat.length;
+		int c = mat[0].length;
+		int i = 0;
+		int[] soldiers = new int[r];
+		int[] ans = new int[k];
+		while (i < r) {
+			if (mat[i][0] == 1) 
+            {
+				int l = 0;
+				int h = c - 1;
+				int res = 0;
+				while (l <= h) 
+                {
+					int mid = (l + h);
+					if (mat[i][mid] == 1) 
+                    {
+						res = mid;
+						l = mid + 1; 
+					} 
+                    else if (mat[i][mid] < 1){
+                        h = mid - 1;
+                    }
+				}
+				soldiers[i] = res + 1; 
+			} 
+            else if (mat[i][0] == 0) 
+            {
+				soldiers[i] = 0;  
+			}
+			i++;
+		}
+		for (int x = 0; x < k; x++) 
+        {
+			int index = 0;
+			int min = c + 1;
+			for (int j = 0; j < soldiers.length; j++) 
+            {
+				if (soldiers[j] < min) 
+                {
+					min = soldiers[j];
+					index = j;
+				}
+			}
+			soldiers[index] = c + 1; 
+			ans[x] = index;
+		}
+		return ans;
+	}
+}
