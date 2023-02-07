0a9f770b097720c01d3c1cd58127320d3fa728d9
priti123-patil <68500888+priti123-patil@users.noreply.github.com>
2023-02-07 22:48:04 +0530

    Time: 56 ms (64.96%), Space: 50.5 MB (86.71%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        //2baskets- each will contain single type of fruit
+        //exactly 1 fruit from 1 tree
+        //hashmap
+        // i freq
+        // 1 1
+        // 2 3
+        // 3 1
+        //longest subarray which has utmost 2 unique subarrays
+        //sliding window - i starting point j-ending point of window
+        HashMap<Integer,Integer> basket=new HashMap<>();
+        int i=0,j=0,max=0;
+        for(j=0;j<fruits.length;j++){
+            basket.put(fruits[j], basket.getOrDefault(fruits[j],0)+1);
+            if(basket.size() > 2){
+                basket.put(fruits[i] , basket.get(fruits[i])-1);//reducethe ith keys value by 1 as we areincrementingi
+            if(basket.get(fruits[i]) == 0)
+                basket.remove(fruits[i]);
+            i++;
+            }
+        }
+        max= j-i;
+        return max;
+    }
+}
