29fb84a14e1bb3778713cd2e5da931b18b0bb73e
priti123-patil <68500888+priti123-patil@users.noreply.github.com>
2023-02-11 22:00:49 +0530

    Time: 1 ms (64.13%), Space: 39.8 MB (78.01%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,57 +1,29 @@
 class Solution {
     public int splitArray(int[] nums, int m) {
-        
-        int start=0;
-        int end=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            start=Math.max(start,nums[i]);
-            end+=nums[i];
+        int low=0 , high=0 , res=0;
+        for(int n:nums){
+            low = Math.max(low , n);
+            high += n;
         }
-        //Maximum value will be in start
-        //end will contain the sum of all elements in an array 
-        //now the start and end will be the limits for adding the elements  
-        //here start and end are the 2 limits 
-        //the sum of the elements in subarrays should not exceed this limit of end
-        
-        
-        while(start<end)
-        {
-            int mid=start+(end-start) / 2;
-            
-            //calcualte the total parts you can divide array
-            //we calculate the mid and set the limit for addition
-            //sum cannt exceed mid 
-            //when it is exceeding make new one and now pieces will increase
-            int sum=0;
-            int pieces=1;
-            
-            for(int num:nums)
+        while(low < high){
+            int mid= low + (high-low) / 2;
+            int sum=0,count=1;
+            for(int n:nums)
             {
-                if(sum  +  num > mid)
-                {
-                 //if this sum is exceeding the limit make new subarray
-                   //ans add that num to next subaray
-                   
-                   sum=num;
-                   pieces++;
-                }
-                else
-                {
-                    sum+=num;
+                if(sum  +  n > mid){
+                    count++;
+                    sum=n;
+                } 
+                else {
+                sum += n;
                 }
             }
-            if(pieces>m)
-            {
-                start=mid+1;
-            }
+            if(count > m)
+                low = mid+1;
             else
-            {
-                end=mid;
-            }
-            
+                high = mid;
         }
-        return end;
-        
+        return high;
     }
+    
 }
