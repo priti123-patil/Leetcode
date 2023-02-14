6eb6f0129c6f81e1c2bb7d2f4e26c09f702cfb1e
priti123-patil <68500888+priti123-patil@users.noreply.github.com>
2023-02-14 22:41:33 +0530

    Time: 43 ms (5.05%), Space: 43.5 MB (5.95%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,78 +1,106 @@
 class Solution {
     public String addBinary(String a, String b) {
-        int ca=a.length();
-		int cb=b.length();
-		if(ca > cb)
-		{
-			int diff= ca-cb;
-			for(int i=0;i<diff;i++)
-			{
-				b = "0" + b ;
-			}
-			return add( a , b );
-		}
-		else if( cb > ca )
-		{
-			int diff= cb-ca;
-			for(int i=0;i<diff;i++)
-			{
-				a = "0" + a ;
-			}
-			return add( a , b );
-		}
-		return add(a,b);
+        char[] arr1=a.toCharArray();
+        char[] arr2=b.toCharArray();
+        Stack<Integer> ans=new Stack<>();
+        int carry=0;
+        char ch1,ch2;
+        int i=a.length()-1,j=b.length()-1;
+        for(i=a.length()-1,j=b.length()-1; i>=0 && j >=0 ; i--,j--)
+        {
+            ch1 = a.charAt(i);
+            ch2 = b.charAt(j);
+            if(ch1=='1' && ch2== '1')
+            {
+                if(carry == 1)
+                {
+                    ans.push(1);
+                }
+                else
+                {
+                    ans.push(0);
+                }
+                System.out.println(0+carry);
+                carry = 1;
+                System.out.println("Case 1 i=1 j=1");
+            }
+            else if(ch1 == '0' && ch2 == '0')
+            {
+                if(carry == 1){
+                    ans.push(1);
+                    carry = 0;
+                }
+                else
+                {
+                    ans.push(0);
+                }
+                System.out.println("Case 2 i=0 j=0");
+                System.out.println(0+carry);
+            }
+            else if(ch1 == '1' && ch2 == '0')
+            {
+                if(carry == 1){
+                    ans.push(0);
+                }
+                else{
+                    ans.push(1);
+                }
+                System.out.println("Case 3 i=1 j=0");
+                System.out.println("carry is "+carry);
+            }
+            else
+            {
+                if(carry == 1){
+                    ans.push(0);
+                }
+                else{
+                    ans.push(1);
+                } // i=0 j=1
+                System.out.println("Case 4 i=0 j=1");
+                System.out.println(1+carry);
+            }
+        }
+        while(i >= 0)
+        {
+            System.out.println("in i loop");
+            if(carry == 1 && a.charAt(i)=='1'){
+                    ans.push(0);
+            }
+            else if(carry == 1 && a.charAt(i)=='0'){
+                    ans.push(1);
+                    carry = 0;
+            }
+            else{
+                ans.push(a.charAt(i)-'0');
+            }
+            i--;
+        }
+        while(j>=0 )
+        {
+            System.out.println("in j loop");
+            if(carry == 1 && b.charAt(j)=='1'){
+                    ans.push(0);
+            }
+            else if(carry == 1 && b.charAt(j)=='0'){
+                    ans.push(1);
+                    carry = 0;
+            }
+            else{
+                    ans.push(b.charAt(j)-'0');
+            }
+            j--;
+        }
+        if(carry == 1)
+        {
+            ans.push(1);
+        }
+        System.out.println(ans);
+        String res="";
+        System.out.println(ans.size());
+        for(int p=0;i < ans.size()-1;p++){
+            Integer k = ans.pop();
+            res += k.toString();
+        }
+        return res;
     }
-    
-    static String add(String a,String b)
-	{
-		int i=a.length()-1;
-		String ans="";
-		int carry = 0;
-		while(i >= 0 )
-		{
-			if(a.charAt(i)=='1' && b.charAt(i)=='1')
-			{
-				if(carry == 1)
-				{
-					ans = "1" + ans;
-					carry = 1;
-				}
-				else
-				{
-					ans = "0" + ans ;
-					carry = 1 ;
-				}
-			}
-else if( (a.charAt(i)=='0' && b.charAt(i)=='1') || ( a.charAt(i)=='1' && b.charAt(i)=='0' )  )
-			{
-				if(carry == 1)
-				{
-					ans = "0" + ans;
-					carry = 1;
-				}
-				else
-				{
-					ans = "1" + ans ;
-				}
-			}
-			else if( a.charAt(i)=='0' && b.charAt(i)=='0')
-			{
-				if(carry == 1)
-				{
-					ans = "1" + ans ;
-					carry = 0;
-				}
-				else
-				{
-					ans = "0" + ans ;
-				}
-			}
-			i--;
-		}
-		if(carry == 1)
-		{
-			ans = "1" + ans ;
-		}
-		return ans;
-	}
 }
