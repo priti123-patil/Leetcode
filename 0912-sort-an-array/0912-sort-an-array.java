37e91551d8623c0bb060c696d39e8a2de7330e7a
priti123-patil <68500888+priti123-patil@users.noreply.github.com>
2023-03-01 19:44:58 +0530

    Time: 41 ms (43.73%), Space: 53.6 MB (44.09%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        if (nums.length == 1) {
+            return nums;
+        }
+
+        int mid = nums.length / 2;
+
+        int[] left = sortArray(Arrays.copyOfRange(nums, 0, mid));
+        int[] right = sortArray(Arrays.copyOfRange(nums, mid, nums.length));
+
+        return merge(left, right);
+    }
+    
+    public int[] merge(int[] left,int[] right)
+    {
+        int[] mix = new int[left.length+right.length];
+        int i=0,j=0,k=0;
+        while(i < left.length && j<right.length){
+            if(left[i] < right[j])
+            {
+                mix[k]= left[i];
+                i++;
+            }
+            else
+            {    
+                mix[k]= right[j];
+                j++;
+            }
+            k++;
+        }
+        while( i <left.length)
+        {
+            mix[k] = left[i];
+            k++;
+            i++;
+        }
+        while( j < right.length)
+        {
+            mix[k] = right[j];
+            k++;
+            j++;
+        }
+        return mix;
+    }
+} 
